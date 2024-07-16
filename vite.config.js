171b0aec52841d6c5ea65c18bcd8f0a0a84853a5
Kirill Mokevnin <mokevnin@gmail.com>
2024-07-16 12:49:52 -0400

    add github pages
    Signed-off-by: Kirill Mokevnin <mokevnin@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import path from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: '/tic-tac-toe-js/',
   build: {
     lib: {
       entry: path.resolve(import.meta.dirname, 'src/main.ts'),
